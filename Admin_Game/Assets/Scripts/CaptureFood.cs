6c73e26942815099450318dc3db5f5c551508b6f
MasterCaio <caiofelipe147@gmail.com>
2016-05-20 17:16:01 -0300

    Adicionando script que destroi os alimentos ao clicar nele

---
+++ Admin_Game/Assets/Scripts/CaptureFood.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaptureFood : MonoBehaviour {
+
+
+	void OnMouseDown(){
+		Destroy(gameObject);
+	}
+
+}
---
+++ Admin_Game/Assets/Scripts/RestaurantManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RestaurantManager : MonoBehaviour {
+
+	public static float money;
+	public Text moneyText;
+
+	// Use this for initialization
+void Start(){
+		money = 5000;
+		moneyText = GameObject.Find("TextValue").GetComponent<Text>();
+		moneyText.text = "Gastos: "+ money + "R$";
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
