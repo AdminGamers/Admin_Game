b866e7e4993297930eeccd3fa8df8ba99b3a49e6
MasterCaio <caiofelipe147@gmail.com>
2016-05-20 20:10:20 -0300

    Adicionando estrutura básica do script que diminui o dinheiro total ao clicar no alimento

--- Admin_Game/Assets/Scripts/CaptureFood.cs
+++ Admin_Game/Assets/Scripts/CaptureFood.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class CaptureFood : MonoBehaviour {
 
+	public static float money = 5000;
+
 
 	void OnMouseDown(){
 		Destroy(gameObject);
+		RestaurantManager.moneyText.text = "Gastos: "+ (money-200) + " R$";
+
 	}
 
 }
--- Admin_Game/Assets/Scripts/RestaurantManager.cs
+++ Admin_Game/Assets/Scripts/RestaurantManager.cs
@@ -4,18 +4,16 @@ using UnityEngine.UI;
 
 public class RestaurantManager : MonoBehaviour {
 
-	public static float money;
-	public Text moneyText;
+	public static Text moneyText;
 
 	// Use this for initialization
 void Start(){
-		money = 5000;
 		moneyText = GameObject.Find("TextValue").GetComponent<Text>();
-		moneyText.text = "Gastos: "+ money + "R$";
+		moneyText.text = "Gastos: "+ CaptureFood.money + "R$";
 	}
 
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
 }
