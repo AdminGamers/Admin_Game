499bcc6f81a2717397f2f83427f5c805f730e090
MasterCaio <caiofelipe147@gmail.com>
2016-05-23 16:14:33 -0300

    Modificando estrutura de funcionamento básica para switch, adicionando lista de compras e prefabs concluidos de todos os alimentos.

--- Admin_Game/Assets/Scripts/CaptureFood.cs
+++ Admin_Game/Assets/Scripts/CaptureFood.cs
@@ -3,17 +3,39 @@ using System.Collections;
 
 public class CaptureFood : MonoBehaviour {
 
-	public static float money = 5000;
+	public static float money=5000;
+	public static int qCarrot = 0;
+	public static int qOnion = 0;
+	public static int qPotato = 0;
+	public static int qBanana = 0;
+	public static int qFruits = 0;
+	public static int qVegetables = 0;
+	public static int qBeans = 0;
+	public static int qChocolate = 0;
+	public static int qBarbecue = 0;
+	public static int qMilk = 0;
+	public static int qCondensedMilk = 0;
+	public static int qSyrup = 0;
+	public static int qEggs = 0;
+	public static int qBacon = 0;
+	public static int qBread = 0;
+	public static int qRice = 0;
+	public static int qFlour = 0;
+	public static int qPig = 0;
+	public static int qChicken = 0;
 
 
 	void OnMouseDown(){
 		Destroy(gameObject);
-        switch (gameObject.tag){ 
+        switch (gameObject.tag){
             case "Carrot":
-                RestaurantManager.moneyText.text = "Gastos: " + (money - 200) + " R$";
+							 	money -= 200;
+								qCarrot += 1;
                 break;
-            default:
-                RestaurantManager.moneyText.text = "Gastos: " + (money - 0) + " R$";
+
+            case "Bacon":
+								money -= 800;
+								qBacon += 1;
                 break;
         }
 	}
--- Admin_Game/Assets/Scripts/RestaurantManager.cs
+++ Admin_Game/Assets/Scripts/RestaurantManager.cs
@@ -4,17 +4,37 @@ using UnityEngine.UI;
 
 public class RestaurantManager : MonoBehaviour {
 
-	public static Text moneyText;
+	public Text moneyText;
+	public Text listText;
 
 	// Use this for initialization
 void Start(){
 		moneyText = GameObject.Find("TextValue").GetComponent<Text>();
-		moneyText.text = "Gastos: "+ CaptureFood.money + " R$";
+		listText = GameObject.Find("ListValue").GetComponent<Text>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-
+		moneyText.text = "Valor total: " + CaptureFood.money + " R$";
+		listText.text = "Você comprou:\n " + CaptureFood.qCarrot + " Cenouras\n "
+		 																	 + CaptureFood.qOnion + " Bacons\n "
+																			 + CaptureFood.qPotato + " Batatas\n "
+																			 + CaptureFood.qBanana + " Bananas\n "
+																			 + CaptureFood.qFruits + " Cestas de frutas\n "
+																			 + CaptureFood.qVegetables + " Cestas de vegetais\n "
+																			 + CaptureFood.qBeans + " Pacotes de feijão\n "
+																			 + CaptureFood.qChocolate + " Chocolates\n "
+																			 + CaptureFood.qBarbecue + " Barbecue\n "
+																			 + CaptureFood.qMilk + " Litros de Leite\n "
+																			 + CaptureFood.qCondensedMilk + " Leite Condensado\n "
+																			 + CaptureFood.qSyrup + " Caldas\n "
+																			 + CaptureFood.qEggs + " Ovos\n "
+																			 + CaptureFood.qBacon + " Bacon\n "
+																			 + CaptureFood.qBread + " Pães\n "
+																			 + CaptureFood.qRice + " Arroz\n "
+																			 + CaptureFood.qFlour + " Sacos de farinha\n "
+																			 + CaptureFood.qPig + " Quilos de porco\n "
+																			 + CaptureFood.qChicken + " Frangos\n ";
 	}
 
 }
