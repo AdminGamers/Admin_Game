fbb267f7f0887f888e3657b3e8537ce3beea450e
MasterCaio <caiofelipe147@gmail.com>
2016-05-26 02:16:22 -0300

    Adicionando valor da cenoura para calculo total

--- Assets/Scripts/ButtonsManager.cs
+++ Assets/Scripts/ButtonsManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ButtonsManager : MonoBehaviour {
 
@@ -7,36 +8,38 @@ public class ButtonsManager : MonoBehaviour {
 	void Start () {
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
 	}
     public double Price(){
-       double price = CaptureFood.qOnion * 2.0 +
-        CaptureFood.qPotato * 2.0 +
-        CaptureFood.qBanana * 2.0 +
-        CaptureFood.qFruits * 4.0 +
-        CaptureFood.qVegetables * 5.50 +
-        CaptureFood.qBeans * 4.50 +
-        CaptureFood.qChocolate * 5.0 +
-        CaptureFood.qBarbecue * 3.50 +
-        CaptureFood.qMilk * 4.0 + 
-        CaptureFood.qCondensedMilk * 4.5 +
-        CaptureFood.qSyrup * 9.0 +
-        CaptureFood.qEggs * 3.0 +
-        CaptureFood.qBacon * 3.0 +
-        CaptureFood.qBread * 3.0 +
-        CaptureFood.qRice * 5.0 +
-        CaptureFood.qFlour * 4.0 +
-        CaptureFood.qPig * 7.0 +
-        CaptureFood.qChicken * 6.5;
+       double price = CaptureFood.qCarrot * 1.50 +
+										  CaptureFood.qOnion * 2.0 +
+							        CaptureFood.qPotato * 2.0 +
+							        CaptureFood.qBanana * 2.0 +
+							        CaptureFood.qFruits * 4.0 +
+							        CaptureFood.qVegetables * 5.50 +
+							        CaptureFood.qBeans * 4.50 +
+							        CaptureFood.qChocolate * 5.0 +
+							        CaptureFood.qBarbecue * 3.50 +
+							        CaptureFood.qMilk * 4.0 +
+							        CaptureFood.qCondensedMilk * 4.5 +
+							        CaptureFood.qSyrup * 9.0 +
+							        CaptureFood.qEggs * 3.0 +
+							        CaptureFood.qBacon * 3.0 +
+							        CaptureFood.qBread * 3.0 +
+							        CaptureFood.qRice * 5.0 +
+							        CaptureFood.qFlour * 4.0 +
+							        CaptureFood.qPig * 7.0 +
+							        CaptureFood.qChicken * 6.5;
         return price;
     }
 	public void Bt_Buy(){
         if (CaptureFood.money > Price()){
-        CaptureFood.money = CaptureFood.money - Price();
-            }
+        	CaptureFood.money = CaptureFood.money - Price();
+					ValuesManager.fCarrot.text = "";
+        }
         Debug.Log("Mudou de Scene");
 		//Application.LoadLevel("");
 	}
--- Assets/Scripts/RestaurantManager.cs
+++ Assets/Scripts/RestaurantManager.cs
@@ -19,24 +19,24 @@ void Start(){
 	void Update () {
 		moneyText.text = "Valor total: " + CaptureFood.money + " R$";
 		listText.text = "Compras:\n\n\n " + CaptureFood.qCarrot + " Cenouras\t\t\t "
-																	+ CaptureFood.qOnion + " Cebolas\n "
-																  + CaptureFood.qPotato + " Batatas\t\t\t\t "
-																  + CaptureFood.qBanana + " Bananas\n "
-																  + CaptureFood.qFruits + " Frutas\t\t\t\t\t "
-																  + CaptureFood.qVegetables + " Vegetais\n "
-																  + CaptureFood.qBeans + " Feijões\t\t\t\t "
-																  + CaptureFood.qChocolate + " Chocolates\n "
-																  + CaptureFood.qBarbecue + " Barbecue\t\t\t "
-																  + CaptureFood.qMilk + " Leites\n "
-																  + CaptureFood.qCondensedMilk + " Leite Condensado\t "
-																  + CaptureFood.qSyrup + " Caldas\n "
-																  + CaptureFood.qEggs + " Ovos\t\t\t\t\t "
-																  + CaptureFood.qBacon + " Bacon\n "
-																  + CaptureFood.qBread + " Pães\t\t\t\t\t "
-																  + CaptureFood.qRice + " Arroz\n "
-																  + CaptureFood.qFlour + " Farinhas\t\t\t\t "
-																  + CaptureFood.qPig + " Porcos\n "
-																  + CaptureFood.qChicken + " Frangos\n ";
+																			+ CaptureFood.qOnion + " Cebolas\n "
+																		  + CaptureFood.qPotato + " Batatas\t\t\t\t "
+																		  + CaptureFood.qBanana + " Bananas\n "
+																		  + CaptureFood.qFruits + " Frutas\t\t\t\t\t "
+																		  + CaptureFood.qVegetables + " Vegetais\n "
+																		  + CaptureFood.qBeans + " Feijões\t\t\t\t "
+																		  + CaptureFood.qChocolate + " Chocolates\n "
+																		  + CaptureFood.qBarbecue + " Barbecue\t\t\t "
+																		  + CaptureFood.qMilk + " Leites\n "
+																		  + CaptureFood.qCondensedMilk + " Leite Condensado\t "
+																		  + CaptureFood.qSyrup + " Caldas\n "
+																		  + CaptureFood.qEggs + " Ovos\t\t\t\t\t "
+																		  + CaptureFood.qBacon + " Bacon\n "
+																		  + CaptureFood.qBread + " Pães\t\t\t\t\t "
+																		  + CaptureFood.qRice + " Arroz\n "
+																		  + CaptureFood.qFlour + " Farinhas\t\t\t\t "
+																		  + CaptureFood.qPig + " Porcos\n "
+																		  + CaptureFood.qChicken + " Frangos\n ";
 	}
 
 }
